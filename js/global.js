3c0757c28b44005fb769a30e6647150e6b0210e5
Nick Johnson <nickj224@gmail.com>
2018-01-28 20:54:42 -0500

    move 2 to default

--- js/global.js
+++ js/global.js
@@ -2,10 +2,12 @@ $(function() {
     // scrolly pants
     $(window).on('scroll', function() {
         const pos = $(window).scrollTop();
-        if (pos > 100) {
+        if (pos > 75) {
             $('header').addClass('scrolled');
+            $('section.p1').addClass('scrolled');
         } else {
             $('header').removeClass('scrolled');
+            $('section.p1').removeClass('scrolled');
         }
     });
 });
